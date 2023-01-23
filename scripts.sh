d4ef4deddbd5da21320647f82c25c4fd60be15a9
kaka <kaka@kaka.com>
2023-01-23 23:59:22 +0300

    Qn7

--- scripts.sh
+++ scripts.sh
@@ -11,7 +11,9 @@ zcat files/sample.vcf.gz |  grep '^[^#]' | cut -f 1,2,6 >> out_dir/answer5.csv
 #6. Extract data that belongs to chromosomes 2,4 and MT
 zcat files/sample.vcf.gz | awk -F "\t" '$1~/^(2|4|MT)$/{print $0}'
 
-7. Print out variants that do not belong to chr20:1-30000000
+#7. Print out variants that do not belong to chr20:1-30000000
+zcat files/sample.vcf.gz |  grep '^[^#]' | awk '!($1 == 20 && $2 >= 1 && $2 <= 30000000)' | wc -l
+
 8. Extract variants that belong to SRR13107019
 9. Filter out variants with a QualByDepth above 7
 10. How many contigs are referred to in the file. Check the header section
