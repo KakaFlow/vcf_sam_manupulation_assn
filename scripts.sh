39f3be71f36d07bb23dc34e9a9b55e9b63834b2e
kaka <kaka@kaka.com>
2023-01-24 09:09:22 +0300

    Qn9

--- scripts.sh
+++ scripts.sh
@@ -15,7 +15,9 @@ zcat files/sample.vcf.gz | awk -F "\t" '$1~/^(2|4|MT)$/{print $0}'
 zcat files/sample.vcf.gz |  grep '^[^#]' | awk '!($1 == 20 && $2 >= 1 && $2 <= 30000000)' | wc -l
 
 8. Extract variants that belong to SRR13107019
-9. Filter out variants with a QualByDepth above 7
+#9. Filter out variants with a QualByDepth above 7
+zcat files/sample.vcf.gz |  grep '^[^#]' | awk 'match($0, /QD=([0-9]+\.[0-9]+)\;/){ $8=substr($0, RSTART, RLENGTH-1) }1' | awk '{ sub("QD=","",$8); print }' | awk '$8>7'
+
 10. How many contigs are referred to in the file. Check the header section
 11. Comment on the eighth and ninth columns of the file
 12. Extract data on the read depth of called variants for sample SRR13107018
